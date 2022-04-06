90951518d14256009598f115d4ba8d2eefd10784
Mindaugas Garnys <mingarnys@gmail.com>
2022-04-06 08:46:38 +0300

    innitial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
+    HeroesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
